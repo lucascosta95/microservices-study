0cc676ca2bf470583692446e3b7252aa70ffa702
Lucas Costa <lucas_costa_95@hotmail.com>
2024-10-21 08:40:09 -0300

    test: add unit tests for findById (second scenario) and findAll in user-service-api

--- user-service-api/src/test/java/br/com/lucascosta/userserviceapi/service/UserServiceTest.java
+++ user-service-api/src/test/java/br/com/lucascosta/userserviceapi/service/UserServiceTest.java
@@ -4,13 +4,16 @@ package br.com.lucascosta.userserviceapi.service;
 import br.com.lucascosta.userserviceapi.entity.User;
 import br.com.lucascosta.userserviceapi.mapper.UserMapper;
 import br.com.lucascosta.userserviceapi.repository.UserRepository;
+import models.exceptions.ResourceNotFoundException;
 import models.responses.UserResponse;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -34,16 +37,57 @@ class UserServiceTest {
     @Mock
     private PasswordEncoder passwordEncoder;
 
-    @Test
-    void whenCallFindByIdWithValidIdThenReturnUserResponse() {
-        when(userRepository.findById(anyString())).thenReturn(Optional.of(new User()));
-        when(userMapper.fromEntity(any(User.class))).thenReturn(mock(UserResponse.class));
+    @Nested
+    class GetUserById {
 
-        final var response = userService.findById("1");
+        @Test
+        void whenCallFindByIdWithValidIdThenReturnUserResponse() {
+            when(userRepository.findById(anyString())).thenReturn(Optional.of(new User()));
+            when(userMapper.fromEntity(any(User.class))).thenReturn(mock(UserResponse.class));
 
-        assertNotNull(response);
-        assertEquals(UserResponse.class, response.getClass());
-        verify(userRepository, times(1)).findById(anyString());
-        verify(userMapper, times(1)).fromEntity(any(User.class));
+            final var response = userService.findById("1");
+
+            assertNotNull(response);
+            assertEquals(UserResponse.class, response.getClass());
+
+            verify(userRepository, times(1)).findById(anyString());
+            verify(userMapper, times(1)).fromEntity(any(User.class));
+        }
+
+        @Test
+        void whenCallFindByIdWithInvalidIdThenThrowResourceNotFoundException() {
+            when(userRepository.findById(anyString())).thenReturn(Optional.empty());
+
+            try {
+                userService.findById("1");
+            } catch (Exception e) {
+                assertEquals(ResourceNotFoundException.class, e.getClass());
+                assertEquals("Object not found. id: 1, type: UserResponse", e.getMessage());
+            }
+
+            verify(userRepository, times(1)).findById(anyString());
+            verify(userMapper, times(0)).fromEntity(any(User.class));
+        }
     }
+
+    @Nested
+    class GetAllUsers {
+
+        @Test
+        void whenCallFindAllThenReturnListOfUserResponse() {
+            when(userRepository.findAll()).thenReturn(List.of(new User(), new User()));
+            when(userMapper.fromEntity(any(User.class))).thenReturn(mock(UserResponse.class));
+
+            final var response = userService.findAll();
+
+            assertNotNull(response);
+            assertEquals(2, response.size());
+            assertEquals(UserResponse.class, response.get(0).getClass());
+
+            verify(userRepository, times(1)).findAll();
+            verify(userMapper, times(1)).fromEntity(any(User.class));
+        }
+    }
+
+
 }
