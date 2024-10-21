2197da1e40d83c9c336cdaa294e22d05212b4690
Lucas Costa <lucas_costa_95@hotmail.com>
2024-10-21 20:41:21 -0300

    test: add unit test for save in user-service-api

--- user-service-api/src/test/java/br/com/lucascosta/userserviceapi/service/UserServiceTest.java
+++ user-service-api/src/test/java/br/com/lucascosta/userserviceapi/service/UserServiceTest.java
@@ -5,17 +5,20 @@ import br.com.lucascosta.userserviceapi.entity.User;
 import br.com.lucascosta.userserviceapi.mapper.UserMapper;
 import br.com.lucascosta.userserviceapi.repository.UserRepository;
 import models.exceptions.ResourceNotFoundException;
+import models.requests.CreateUserRequest;
 import models.responses.UserResponse;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.List;
 import java.util.Optional;
 
+import static br.com.lucascosta.userserviceapi.creator.CreatorUtils.generateMock;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
@@ -39,19 +42,18 @@ class UserServiceTest {
 
     @Nested
     class GetUserById {
-
         @Test
         void whenCallFindByIdWithValidIdThenReturnUserResponse() {
             when(userRepository.findById(anyString())).thenReturn(Optional.of(new User()));
-            when(userMapper.fromEntity(any(User.class))).thenReturn(mock(UserResponse.class));
+            when(userMapper.fromEntity(any(User.class))).thenReturn(generateMock(UserResponse.class));
 
             final var response = userService.findById("1");
 
             assertNotNull(response);
             assertEquals(UserResponse.class, response.getClass());
 
-            verify(userRepository, times(1)).findById(anyString());
-            verify(userMapper, times(1)).fromEntity(any(User.class));
+            verify(userRepository).findById(anyString());
+            verify(userMapper).fromEntity(any(User.class));
         }
 
         @Test
@@ -65,14 +67,13 @@ class UserServiceTest {
                 assertEquals("Object not found. id: 1, type: UserResponse", e.getMessage());
             }
 
-            verify(userRepository, times(1)).findById(anyString());
+            verify(userRepository).findById(anyString());
             verify(userMapper, times(0)).fromEntity(any(User.class));
         }
     }
 
     @Nested
     class GetAllUsers {
-
         @Test
         void whenCallFindAllThenReturnListOfUserResponse() {
             when(userRepository.findAll()).thenReturn(List.of(new User(), new User()));
@@ -84,10 +85,45 @@ class UserServiceTest {
             assertEquals(2, response.size());
             assertEquals(UserResponse.class, response.get(0).getClass());
 
-            verify(userRepository, times(1)).findAll();
-            verify(userMapper, times(1)).fromEntity(any(User.class));
+            verify(userRepository).findAll();
+            verify(userMapper).fromEntity(any(User.class));
         }
     }
 
+    @Nested
+    class SaveUser {
+        @Test
+        void whenCallSaveUserThenSuccess() {
+            final var request = generateMock(CreateUserRequest.class);
+            when(userMapper.fromRequest(any())).thenReturn(new User());
+            when(passwordEncoder.encode(anyString())).thenReturn("encoded");
+            when(userRepository.save(any(User.class))).thenReturn(new User());
+            when(userRepository.findByEmail(anyString())).thenReturn(Optional.empty());
 
+            userService.save(request);
+
+            verify(userMapper).fromRequest(request);
+            verify(passwordEncoder).encode(request.password());
+            verify(userRepository).save(any(User.class));
+            verify(userRepository).findByEmail(request.email());
+        }
+
+        @Test
+        void whenCallSaveUserWithInvalidEmailThenThrowDataIntegrityViolationException() {
+            final var request = generateMock(CreateUserRequest.class);
+            when(userRepository.findByEmail(anyString())).thenReturn(Optional.of(generateMock(User.class)));
+
+            try {
+                userService.save(request);
+            } catch (Exception e) {
+                assertEquals(DataIntegrityViolationException.class, e.getClass());
+                assertEquals(String.format("Email [ %s ]  already exists", request.email()), e.getMessage());
+            }
+
+            verify(userRepository).findByEmail(request.email());
+            verify(userMapper, times(0)).fromRequest(request);
+            verify(passwordEncoder, times(0)).encode(request.password());
+            verify(userRepository, times(0)).save(any(User.class));
+        }
+    }
 }
