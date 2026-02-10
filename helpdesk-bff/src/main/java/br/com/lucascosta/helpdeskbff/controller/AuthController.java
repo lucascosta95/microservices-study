11427f3addebce8607309935e5a3ea0bbe05ca91
Lucas Costa <lucas_costa_95@hotmail.com>
2026-02-09 23:09:17 -0300

    feat: add Swagger @Tag annotation to AuthController and re-enable JWT dependencies in helpdesk-bff

--- helpdesk-bff/src/main/java/br/com/lucascosta/helpdeskbff/controller/AuthController.java
+++ helpdesk-bff/src/main/java/br/com/lucascosta/helpdeskbff/controller/AuthController.java
@@ -6,6 +6,7 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import models.exceptions.StandardError;
 import models.requests.AuthenticateRequest;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
+@Tag(name = "AuthController", description = "Controller responsible for authentication operations")
 @RequestMapping("/api/auth")
 public interface AuthController {
 
